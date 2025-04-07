102cdfd568b51fc3ddb2cc1c6caa7dcb18bd2fe0
Aaron Park <aaronj.park0304@gmail.com>
2025-04-06 19:10:38 -0500

    Implement ExpenseForm and Input components for expense management; replace old Input component and integrate ExpenseForm into ManageExpense screen.

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -7,6 +7,7 @@ import { GlobalStyles } from "../constants/styles";
 
 import IconButton from "../components/UI/IconButton";
 import Button from "../components/UI/Button";
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 
 function ManageExpense({ route, navigation }) {
   const expensesCtx = useContext(ExpensesContext);
@@ -48,7 +49,7 @@ function ManageExpense({ route, navigation }) {
 
   return (
     <View style={styles.container}>
-  
+      <ExpenseForm />
       <View style={styles.buttons}>
         <Button style={styles.button} mode="flat" onPress={cancelHandlers}>
           Cancel
